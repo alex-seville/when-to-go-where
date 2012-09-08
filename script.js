b638c11f5d25d1e6029a5ef16a6848be89c5fba2
alex-seville <alexander.southern@gmail.com>
2012-09-08 12:03:45 -0400

    Initial commit with base code

---
+++ script.js
@@ -0,0 +1,39 @@
+var chooser = {
+	choices: []
+};
+
+$( function () {
+	$( "div.criteria" ).on( "click", "fieldset", function() {
+		var $t = $( this );
+		if ( $t.hasClass( "off" ) ) {
+			$t.removeClass( "off" ).addClass( "on" );
+		} else {
+			$t.addClass( "off" ).removeClass( "on" );
+		}
+	});
+	$( "div.criteria" ).on( "click", "label", function( e ) {
+		e.stopPropagation();
+		var $t = $( this ),
+			cname = $t.find( "input" ).val(),
+			sibs = $t.siblings( "label" ),
+			removeIndex,
+			classes;
+		$( sibs ).each( function() {
+			removeIndex = chooser.choices.indexOf( $( this ).find( "input" ).val() );
+			if ( removeIndex > -1 ) chooser.choices.splice( removeIndex, 1 );
+		});
+		if ( cname.length ) {
+			if ( cname.indexOf( "." ) > -1 ) {
+				$.each( cname.split( "." ), function( i, nm ) {
+					chooser.choices.push( nm );
+				});
+			} else {
+				chooser.choices.push( cname );
+			}
+		}
+		if ( !chooser.choices.length ) return;
+		classes = chooser.choices.join( "." );
+		$( "div.engines div:not(." + classes + ")" ).addClass( "remove" ).removeClass( "add" );
+		$( "div.engines div." + classes ).addClass( "add" ).removeClass( "remove" );
+	});
+});
