680104a0e045ac07f274d99f5345067e43a7c824
alex-seville <alexander.southern@gmail.com>
2012-09-08 16:55:50 -0400

    Added preliminary, unsourced, destination list Added templating for destination rows Updated html to add table of destinations Updated filters Added reset to filters

---
+++ destinations.js
@@ -0,0 +1,762 @@
+var destinationData = [
+{
+    name: "Peru",
+    classes: "sun south-america",
+    months: {
+        3: "good",
+        4: "good",
+        5: "good"
+    },
+    info: {
+        source: "wikitravel",
+        url: "http://en.wikipedia.org/wiki/Climate_of_Peru"
+    }
+},
+{
+    name: "Venezuela",
+    classes: "sun south-america",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        3: "good",
+        4: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Uruguay",
+    classes: "sun south-america",
+    months: {
+        3: "good",
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good",
+        8: "good"
+    }
+},
+{
+    name: "Ecuador",
+    classes: "sun south-america",
+    months: {
+        0: "good",
+        1: "good",
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Chile",
+    classes: "sun south-america",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        9: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Brazil",
+    classes: "sun south-america",
+    months: {
+        1: "good",
+        2: "good",
+        3: "good",
+        4: "good",
+        5: "good"
+    }
+},
+{
+    name: "Bolivia",
+    classes: "sun south-america",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        3: "good",
+        4: "good",
+        8: "good",
+        9: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Argentina",
+    classes: "sun south-america",
+    months: {
+        1: "good",
+        2: "good",
+        3: "good"
+    }
+},
+{
+    name: "Algeria",
+    classes: "sun africa",
+    months: {
+        5: "good",
+        6: "good"
+    }
+},
+{
+    name: "Egypt",
+    classes: "sun africa",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        3: "good",
+        9: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Morocco",
+    classes: "sun africa",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        3: "good",
+        4: "good",
+        5: "good",
+        9: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Tunisia",
+    classes: "sun africa",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        3: "good",
+        4: "good",
+        5: "good",
+        8: "good",
+        9: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Mauritania",
+    classes: "sun africa",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Benin",
+    classes: "sun africa",
+    months: {
+        0: "good",
+        1: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Burkina Faso",
+    classes: "sun africa",
+    months: {
+        0: "good",
+        1: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Cameroon",
+    classes: "sun africa",
+    months: {
+        2: "good",
+        10: "good"
+    }
+},
+{
+    name: "Togo",
+    classes: "sun africa",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        3: "good",
+        9: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Sierra Leone",
+    classes: "sun africa",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        3: "good",
+        9: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Ghana",
+    classes: "sun africa",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        3: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Zambia",
+    classes: "sun africa",
+    months: {
+        6: "good",
+        7: "good",
+        8: "good",
+        9: "good"
+    }
+},
+{
+    name: "Uganda",
+    classes: "sun africa",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        6: "good",
+        7: "good",
+        8: "good"
+    }
+},
+{
+    name: "Tanzania",
+    classes: "sun africa",
+    months: {
+        6: "good",
+        7: "good",
+        8: "good"
+    }
+},
+{
+    name: "Swaziland",
+    classes: "sun africa",
+    months: {
+        7: "good",
+        8: "good"
+    }
+},
+{
+    name: "South Africa",
+    classes: "sun africa",
+    months: {
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good",
+        8: "good"
+    }
+},
+{
+    name: "Botswana",
+    classes: "sun africa",
+    months: {
+        2: "good",
+        3: "good",
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good"
+    }
+},
+{
+    name: "Italy",
+    classes: "sun europe",
+    months: {
+        2: "good",
+        3: "good",
+        4: "good",
+        5: "good",
+        8: "good"
+    }
+},
+{
+    name: "Spain",
+    classes: "sun europe",
+    months: {
+        4: "good",
+        5: "good",
+        8: "good"
+    }
+},
+{
+    name: "England",
+    classes: "rain europe",
+    months: {
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good",
+        8: "good"
+    }
+},
+{
+    name: "Scotland",
+    classes: "rain europe",
+    months: {
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good",
+        8: "good"
+    }
+},
+{
+    name: "Whales",
+    classes: "rain europe",
+    months: {
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good",
+        8: "good"
+    }
+},
+{
+    name: "Ireland",
+    classes: "rain europe",
+    months: {
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good",
+        8: "good"
+    }
+},
+{
+    name: "Switzerland",
+    classes: "snow europe",
+    months: {
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good"
+    }
+},
+{
+    name: "Austria",
+    classes: "snow europe",
+    months: {
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good"
+    }
+},
+{
+    name: "Turkey",
+    classes: "sun europe middle-east",
+    months: {
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good"
+    }
+},
+{
+    name: "Greece",
+    classes: "sun europe",
+    months: {
+        3: "good",
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good",
+        8: "good"
+    }
+},
+{
+    name: "Slovenia",
+    classes: "europe",
+    months: {
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good"
+    }
+},
+{
+    name: "Germany",
+    classes: "europe",
+    months: {
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good"
+    }
+},
+{
+    name: "Czech Republic",
+    classes: "rain europe",
+    months: {
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good"
+    }
+},
+{
+    name: "Thailand",
+    classes: "rain sun asia",
+    months: {
+        0: "good",
+        1: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Singapore",
+    classes: "rain sun asia",
+    months: {
+        1: "good",
+        2: "good",
+        3: "good",
+        4: "good"
+    }
+},
+{
+    name: "Malaysia",
+    classes: "rain sun asia",
+    months: {
+        2: "good",
+        3: "good",
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good",
+        8: "good"
+    }
+},
+{
+    name: "Laos",
+    classes: "rain sun asia",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        3: "good",
+        4: "good",
+        11: "good",
+        12: "good"
+    }
+},
+{
+    name: "Indonesia",
+    classes: "rain sun asia",
+    months: {
+        3: "good",
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good",
+        8: "good",
+        9: "good"
+    }
+},
+{
+    name: "Cambodia",
+    classes: "rain sun asia",
+    months: {
+        10: "good",
+        11: "good",
+        0: "good"
+    }
+},
+{
+    name: "Brunei",
+    classes: "rain sun asia",
+    months: {
+        3: "good",
+        4: "good",
+        5: "good",
+        6: "good",
+        7: "good",
+        8: "good",
+        9: "good"
+    }
+},
+{
+    name: "Nepal",
+    classes: "asia",
+    months: {
+        3: "good",
+        4: "good",
+        5: "good",
+        9: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "India",
+    classes: "asia",
+    months: {
+        3: "good",
+        4: "good",
+        5: "good",
+        9: "good",
+        1: "good",
+        2: "good"
+    }
+},
+{
+    name: "Bhutan",
+    classes: "asia",
+    months: {
+        3: "good",
+        4: "good",
+        5: "good",
+        9: "good",
+        10: "good"
+    }
+},
+{
+    name: "Bangladesh",
+    classes: "asia",
+    months: {
+        2: "good",
+        3: "good",
+        4: "good",
+        9: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Taiwan",
+    classes: "asia",
+    months: {
+        10: "good",
+        11: "good",
+        12: "good"
+    }
+},
+{
+    name: "South Korea",
+    classes: "asia",
+    months: {
+        2: "good",
+        3: "good",
+        4: "good",
+        5: "good",
+        8: "good",
+        9: "good",
+        10: "good"
+    }
+},
+{
+    name: "Mongolia",
+    classes: "asia",
+    months: {
+        4: "good",
+        5: "good",
+        8: "good"
+    }
+},
+{
+    name: "China",
+    classes: "asia",
+    months: {
+        2: "good",
+        3: "good",
+        4: "good",
+        8: "good",
+        9: "good"
+    }
+},
+{
+    name: "Japan",
+    classes: "asia",
+    months: {
+        4: "good",
+        5: "good",
+        9: "good",
+        10: "good"
+    }
+},
+{
+    name: "Vietnam",
+    classes: "asia",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        11: "good"
+    }
+},
+{
+    name: "Australia",
+    classes: "australia-oceana",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        3: "good",
+        4: "good",
+        5: "good",
+        9: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "New Zealand",
+    classes: "australia-oceana",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        3: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Syria",
+    classes: "middle-east",
+    months: {
+        2: "good",
+        3: "good",
+        4: "good"
+    }
+},
+{
+    name: "Jordan",
+    classes: "middle-east",
+    months: {
+        2: "good",
+        3: "good",
+        4: "good"
+    }
+},
+{
+    name: "Lebanon",
+    classes: "middle-east",
+    months: {
+        2: "good",
+        3: "good",
+        4: "good"
+    }
+},
+{
+    name: "Israel",
+    classes: "middle-east",
+    months: {
+        2: "good",
+        3: "good",
+        4: "good"
+    }
+},
+{
+    name: "Belize",
+    classes: "central-america",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        11: "good"
+    }
+},
+{
+    name: "Costa Rica",
+    classes: "central-america",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        3: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "El Salvador",
+    classes: "central-america",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Guatemala",
+    classes: "central-america",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Mexico",
+    classes: "north-america",
+    months: {
+        0: "good",
+        1: "good",
+        2: "good",
+        10: "good",
+        11: "good"
+    }
+},
+{
+    name: "Canada",
+    classes: "north-america",
+    months: {
+        0: "cold",
+        1: "cold",
+        2: "cold",
+        5: "good",
+        6: "hot",
+        7: "good",
+        10: "cold",
+        11: "cold"
+    }
+},
+{
+    name: "United States",
+    classes: "north-america",
+    months: {
+        0: "cold",
+        1: "cold",
+        5: "good",
+        6: "good",
+        7: "good",
+        11: "cold"
+    }
+}
+];
--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+//Fill destinations first
+
+var results = document.getElementById("destList");
+results.innerHTML = tmpl("dest_tmpl", {destinations: destinationData});
+
+//Then set up filtering
 var chooser = {
 	choices: []
 };
 
 $( function () {
-	$( "div.criteria" ).on( "click", "fieldset", function() {
+	$( "div.criteria" ).on( "click", "fieldset .resetFilters", function(event) {
+		event.stopPropagation();
+		var $t = $( this );
+		var $criteria = $t.closest(".criteria");
+		$criteria.find("fieldset.on" ).addClass("off").removeClass("on");
+		$criteria.find("input:checked").attr('checked', false);
+		$( "table.destinations tr").stop().fadeIn();
+		chooser.choices= [];
+	});
+	$( "div.criteria" ).on( "click", "fieldset:not(.neutral)", function() {
 		var $t = $( this );
 		if ( $t.hasClass( "off" ) ) {
 			$t.removeClass( "off" ).addClass( "on" );
@@ -28,12 +43,14 @@ $( function () {
 					chooser.choices.push( nm );
 				});
 			} else {
-				chooser.choices.push( cname );
+				if (chooser.choices.indexOf(cname) === -1){
+					chooser.choices.push( cname );
+				}
 			}
 		}
 		if ( !chooser.choices.length ) return;
 		classes = chooser.choices.join( "." );
-		$( "div.engines div:not(." + classes + ")" ).addClass( "remove" ).removeClass( "add" );
-		$( "div.engines div." + classes ).addClass( "add" ).removeClass( "remove" );
+		$( "table.destinations tbody tr:not(." + classes + ")" ).stop().fadeOut();
+		$( "table.destinations tbody tr." + classes ).stop().fadeIn();
 	});
 });
